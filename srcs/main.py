9c09f9803b3ee83a729c666309ca9c73e148f89c
jihkang <106561135+jihkang@users.noreply.github.com>
2024-06-07 15:36:54 +0900

    created todo

--- srcs/main.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-import sys
-import os
-
-
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", reload=True)
